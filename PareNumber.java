e2a0a5d44bf0c40ba148a16aafe71d1678a62293
進之介 <sin@sakiharashinnosuke.local>
2016-04-29 17:00:44 +0900

    変更

--- PareNumber.java
+++ PareNumber.java
@@ -1,1 +1,44 @@
-//
+
+package G7.PareNumber;
+import java.awt.event.*;
+import java.swing.*;
+
+public class PareNumberView extends JFrame{
+    private PareNumberController controller;
+    private PareNumberModel Model;
+    private PareNumberPanel Panel;
+}
+
+public static void main(String[] args){
+    new PareNumber();
+}
+
+pubilc PareNumber(){
+    int x = 7;
+    int y = 7;
+    this.getContentPane().setLayout(null);
+    this.setSize(x * 50 + 50, y * 50 + 100);
+    this.setResizable(false);
+    Model = new PareNumberModel(x,y);
+    Panel = new PareNumeberModel(x,y,Model);
+    controller = new PareNumberController(Panel,Model);
+    Panel.setController(controller);
+    Panel.setBounds(25.25,x*50,y*50);
+    this.getContentPane().add(Panel);
+
+    this.makeMenuBer();
+    this.setDefaultCloseOperetion(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(ture);
+}
+
+public void makeMenuBar(){
+    JMenuBar bar = new JMenuBar();
+    JMenu menu = new JMenu("ゲーム");
+    JMenuItem newitem = new JMenuItem("ゲームを始める");
+    newitem.addActionListener(new ActionListener(){
+	    pubilc void actionPerformed(ActionEvent ev){
+		controller.newGame();
+	    }
+	});
+}
+
