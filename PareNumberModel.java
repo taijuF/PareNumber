164199b789bc7e2a6ec785719dc784e5feb11aa9
佐藤 義龍 <giru@satou-no-MacBook-Air.local>
2016-04-29 18:09:07 +0900

    4/29最終

--- PareNumberModel.java
+++ PareNumberModel.java
@@ -2,30 +2,49 @@
 import java.util.*;
 
 public class PareNumberModel{
+    
+    int MaxNumber = 0; //難易度管理
+    int PanelNumber[][];//パネルの数時管理
 
-    public void PareNumberModel(){
+    public PareNumberModel(){
         
-}
+    }
     
-public int easy(){ //マスの数を送り返す
-    return 4;
-}
+    public int easy(){ //マスの数を送り返す(仮)
+        MaxNumber = 4;
+        return 4;
+    }
     
-public int nomal(){
-    return 6;
-}
+    public int nomal(){
+        MaxNumber = 6;
+        return 6;
+    }
     
-public int hard(){
-    return 8;
-}    public void numberClear(){
+    public int hard(){
+        MaxNumber = 8;
+        return 8;
+    }
+    
+    public void getNumber(){//数字を入れた配列を返す(初期設定)
+        for(int i=0;i < MaxNumber;i++){
+            for(int j=0;j < MaxNumber;j++){
+                Random rnd = new Random();
+                int n = rnd.nextInt(MaxNumber+1);
+                PanelNumber[i][j] = n;
+            }
+        }//配列を返す手段募集中
+    }
+    
+    public void numberClear(){
         
-    //入力を受けてその左右のマスのチェックと数字の消去
-    //消すことができたならnumberFallへ
-}
+        //入力を受けてその左右のマスのチェックと数字の消去
+        //消すことができたならnumberFallへ
+    }
     
-private void numberFall(){
-    //空いてる部分にランダムに数字を入れる
+    private void numberFall(){
+        //空いてる部分にランダムに数字を入れる
+        //空いている場所の判定
         
-}
+    }
 
 }
