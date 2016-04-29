1b93af636d4c4d717f4a6a6247ac02896fb3c886
佐藤 義龍 <giru@satou-no-MacBook-Air.local>
2016-04-29 17:04:32 +0900

    modelmethod

--- PareNumberModel.java
+++ PareNumberModel.java
@@ -1,1 +1,31 @@
+//model
+import java.util.*;
+
+public class test
+    public test(){
+        
+}
+    
+public int easy(){ //マスの数を送り返す
+    return 4;
+}
+    
+public int nomal(){
+    return 6;
+}
+    
+public int hard(){
+    return 8;
+}    public void numberClear(){
+        
+    //入力を受けてその左右のマスのチェックと数字の消去
+    //消すことができたならnumberFallへ
+}
+    
+private void numberFall(){
+    //空いてる部分にランダムに数字を入れる
+        
+}
+
+}
 //
