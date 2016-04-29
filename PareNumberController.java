b9b057925dab8fdfe67651db54e006d2c9c1af70
taijuF <taiju4451@gmail.com>
2016-04-29 17:34:51 +0900

    コントローラ書き込み

--- PareNumberController.java
+++ PareNumberController.java
@@ -1,1 +1,26 @@
-//
+//controller
+import java.awt.*;
+import javax.swing.*;
+
+public class PareNumberController{
+    PareNumberPanel panel;
+    PareNumberModel model;
+
+    public PareNumberController(PareNumberPanel p, PareNumberModel m){
+
+	panel = p;
+	model = m;
+	//panel.repaint();
+
+    }
+
+    public void didClick(int x, int y){
+	//処理
+    }
+
+    public void didType(int x, int y){
+	//文字判定とか
+    }
+
+}
+
