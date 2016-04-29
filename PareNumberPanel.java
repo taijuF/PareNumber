df6cd8e55e604efaa5c9e946caeb372553143b9c
小池智也 <koiketomoya@koiketomoya-no-MacBook-Air.local>
2016-04-29 17:30:34 +0900

    paneltest

--- PareNumberPanel.java
+++ PareNumberPanel.java
@@ -1,1 +1,18 @@
-//
+//panel
+
+import javax.swing.*;
+
+public class PareNumberPanel extends JPanel {
+    int width, height;
+    // PareNumberModel model;
+    // PareNumberController controller;
+
+    static int gridWid, gridHgt;
+
+    private static void setGridSize(int w, int h) {
+	gridWid = w;
+	gridHgt = h;
+    }
+
+}
+
