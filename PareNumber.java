2f4109ffda503b1fcbce5a9d9ec8bce213b511b2
taijuF <taiju4451@gmail.com>
2016-05-13 17:19:49 +0900

    aaa

--- PareNumber.java
+++ PareNumber.java
@@ -1,56 +1,78 @@
 
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
 
 public class PareNumber extends JFrame{
-    private PareNumberController controller;
+    /*  
+  private PareNumberController controller;
     private PareNumberModel Model;
     private PareNumberPanel Panel;
-    
-    public static void main(String[] args){
-       new PareNumber();
-   }
+    */
 
-   public PareNumber(){
-       int x = 7;
-       int y = 7;
-       this.getContentPane().setLayout(null);
-       this.setSize(x * 50 + 50, y * 50 + 100);
-       this.setResizable(false);
-       Model = new PareNumberModel(x,y);
-       Panel = new PareNumeberModel(x,y,Model);
-       controller = new PareNumberController(Panel,Model);
-       Panel.setController(controller);
-       Panel.setBounds(25.25,x*50,y*50);
-       this.getContentPane().add(Panel);
-
-       this.makeMenuBer();
-       this.setDefaultCloseOperetion(JFrame.EXIT_ON_CLOSE);
-       this.setVisible(ture);
+    public static void main(String args[]){
+       JFrame frame = new JFrame("PareNumber");
+       frame.setSize(600,600);
+       frame.setLayout(null);
+       PareNumberPanel panel = new PareNumberPanel();
+       //JPanel p = new JPanel();
+       //p.setBackground(Color.RED);
+       panel.setSize(300,300);
+       frame.add(panel);
+       //frame.add(p);
+       frame.setVisible(true);
    }
 
    public void makeMenuBar(){
        JMenuBar bar = new JMenuBar();
        JMenu menu = new JMenu("ゲーム");
        bar.add(menu);
-       JMenuItem newitem1 = new JMenuItem("easy");
-       JMenuItem newitem2 = new JMenuItem("normal");
-       JMenuItem newitem3 = new JMenuItem("diffcult");  
-       JMenuItem newitem4 = new JMenuItem("score");  
-       JMenuItem newitem5 = new JMenuItem("close");
-       menu.add(newitem1);
-       menu.add(newitem2);
-       menu.add(newitem3);
-       menu.add(newitem4);
-       menu.add(newitem5);
-       setJMenuBar(bar);
-
-
-       // newitem.addActionListener(new ActionListener(){
-         /*  pubilc void actionPerformed(ActionEvent ev){
-              controller.newGame();
-          }*/
-      //});
+       JMenuItem easyitem = new JMenuItem("easy");
+       /*
+	 easy.addActionListener(new ActionListener(){
+	 pubilc void actionPerformed(ActionEvent ev){
+	 controller.easy();
+	 }
+	 });
+       */
+       menu.add(easyitem);
+       JMenuItem normalitem = new JMenuItem("normal");
+       /*
+	 normal.addActionListener(new ActionListener(){
+	 pubilc void actionPerformed(ActionEvent ev){
+	 controller.normal();
+	 }
+	 });
+       */
+       menu.add(normalitem);
+       JMenuItem diffcultitem = new JMenuItem("diffcult");  
+       /*
+	 easy.addActionListener(new ActionListener(){
+	 pubilc void actionPerformed(ActionEvent ev){
+	 controller.difcult();
+	 }
+	 });
+       */     
+       menu.add(diffcultitem);
+       JMenuItem scoreitem = new JMenuItem("score");  
+       /*
+	 easy.addActionListener(new ActionListener(){
+	 pubilc void actionPerformed(ActionEvent ev){
+	 controller.score();
+	 }
+	 });
+       */
+       menu.add(scoreitem);
+       JMenuItem quititem = new JMenuItem("Quit");
+       /*
+       easy.addActionListener(new ActionListener(){
+       pubilc void actionPerformed(ActionEvent ev){
+       System.exit(0);
+       }
+       });
+       */
+       menu.add(quititem);
+       bar.add(menu);
+       this.setJMenuBar(bar);
    }
-
 }
