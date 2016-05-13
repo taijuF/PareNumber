458c37eabc93adec67cd52ef010cc73210de7bcb
taijuF <taiju4451@gmail.com>
2016-05-13 17:03:04 +0900

    panel

--- PareNumberPanel.java
+++ PareNumberPanel.java
@@ -1,24 +1,15 @@
-//panel
+////////panel
 
-import java. awt.*;
-
-class MyFrame extends Frame{
-    public MyFrame(String name){
-	setLocation(200, 100);
-	setSize(600, 600);
-	setBackground(Color.WHITE);
+import java.awt.*;
+import javax.swing.*;
+class PareNumberPanel extends JFrame{
+    //    int width=600;
+    //int hight=600;
+    PareNumberPanel(){
+	super();
+	setBackground(Color.BLACK);
     }
     
-    public void paint(Graphics g){
-	g.drawString("数字ペア消しゲーム", 230, 50);
-	g.drawString("Start!", 270, 200);
-    } 
 }
 
-public class PareNumberPanel{
-    public static void main(String[] args){
-	MyFrame myFrame = new MyFrame("PareNumber");
-	myFrame.setVisible(true);
-    }
-}
 
