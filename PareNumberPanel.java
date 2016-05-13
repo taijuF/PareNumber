f108e85c62d3e66a1a138022bb31d9aa3426bfd7
小池智也 <koiketomoya@koiketomoya-no-MacBook-Air.local>
2016-05-13 15:22:34 +0900

    update

--- PareNumberPanel.java
+++ PareNumberPanel.java
@@ -1,18 +1,24 @@
 //panel
 
-import javax.swing.*;
+import java. awt.*;
 
-public class PareNumberPanel extends JPanel {
-    int width, height;
-    // PareNumberModel model;
-    // PareNumberController controller;
-
-    static int gridWid, gridHgt;
-
-    private static void setGridSize(int w, int h) {
-	gridWid = w;
-	gridHgt = h;
+class MyFrame extends Frame{
+    public MyFrame(String name){
+	setLocation(200, 100);
+	setSize(600, 600);
+	setBackground(Color.WHITE);
     }
+    
+    public void paint(Graphics g){
+	g.drawString("数字ペア消しゲーム", 230, 50);
+	g.drawString("Start!", 270, 200);
+    } 
+}
 
+public class PareNumberPanel{
+    public static void main(String[] args){
+	MyFrame myFrame = new MyFrame("PareNumber");
+	myFrame.setVisible(true);
+    }
 }
 
