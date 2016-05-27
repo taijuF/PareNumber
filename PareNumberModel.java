9421bcaa6f329f6927cd3275158f691320ddc1b8
佐藤 義龍 <giru@satou-no-MacBook-Air.local>
2016-05-27 15:49:50 +0900

    model

--- PareNumberModel.java
+++ PareNumberModel.java
@@ -6,13 +6,15 @@ public class PareNumberModel{
     int MaxNumber = 0; //大きさ管理
     int PanelNumber[][];//パネルの数字管理
     int a,b,c,d;
+    int count=0;
     
     public PareNumberModel(){
         
     }
     
-    public void selectDifficulty(){
+    public void selectDifficulty(){ //viewに
         //this.viewの難易度選択画面のメソッド
+        //未完成？
     }
     
     public int easy(){ //マスの数を送り返す(仮)
@@ -33,28 +35,29 @@ public class PareNumberModel{
         return 8;//メソッド呼び出し？
     }
     
-    public void getNumber(){//数字を入れた配列を返す(初期設定)
+    public int[][] getNumber(){//数字を入れた配列を返す(初期設定)
         for(int i = 0;i <= MaxNumber;i++){
             for(int j = 0;j <= MaxNumber;j++){
                 Random rnd = new Random();
                 int n = rnd.nextInt(MaxNumber)+1;
                 PanelNumber[i][j] = n;
             }
-        }//配列を返す手段募集中
+        }
+        return PanelNumber;
     }
     
-    public void numberClear(int a1,int b1,int c1,int d1){ //a,c = X  b,d = Y　0のある配列
-        a = a1;
-        b = b1;
-        c = c1;
-        d = d1;
+    public int[][] numberClear(int x1,int y1,int x2,int y2){ //a,c = X  b,d = Y　0のある配列
+        a = x1;
+        b = y1;
+        c = x2;
+        d = y2;
         if(PanelNumber[a][b] == PanelNumber[c][d]){ //選ばれた二つの数字が同じならば
-            int CheckNember; //チェックナンバーを用意
+            int CheckNumber; //チェックナンバーを用意
             for(int i = b;i >= 0;i--){ //aの上のチェック
-                CheckNember = PanelNumber[a][i];
+                CheckNumber = PanelNumber[a][i];
                 
                 for(int j=i-1;j >= 0;j--){ //チェックする位置の移動
-                    if(CheckNember != PanelNumber[a][j]){ //違う数字が出たらループから抜ける
+                    if(CheckNumber != PanelNumber[a][j]){ //違う数字が出たらループから抜ける
                         break;
                     }
                     PanelNumber[a][j+1] = 0;
@@ -63,10 +66,10 @@ public class PareNumberModel{
             }
             
             for(int i = b;i >= 0;i--){ //cの上のチェック
-                CheckNember = PanelNumber[c][i];
+                CheckNumber = PanelNumber[c][i];
                 
                 for(int j=i-1;j >= 0;j--){
-                    if(CheckNember != PanelNumber[c][j]){
+                    if(CheckNumber != PanelNumber[c][j]){
                         break;
                     }
                     PanelNumber[c][j+1] = 0;
@@ -75,13 +78,16 @@ public class PareNumberModel{
             }
             PanelNumber[a][b] = 0;
             PanelNumber[c][d] = 0;
+            
+            numberFall(); //消去するところまでが一連の動作
         }
-        //配列を返す手段
+        //一番外側
+        return PanelNumber;
     }
     
     
     
-    public void numberFall(){ //０のない配列
+    public int[][] numberFall(){ //０のない配列
         for(int i = b;i >= 0;i--){ //aの上のチェック
             int j = 0;
             int checkupNumber = 0;
@@ -102,7 +108,7 @@ public class PareNumberModel{
                         Random rnd = new Random();
                         int n = rnd.nextInt(MaxNumber+1);
                         PanelNumber[a][i] = n;
-                        
+                        count++;
                     }
                     break;
                 }
@@ -130,24 +136,28 @@ public class PareNumberModel{
                         Random rnd = new Random();
                         int n = rnd.nextInt(MaxNumber+1);
                         PanelNumber[c][i] = n;
-                        
+                        count++;
                     }
                     break; //外側ループから抜ける
                 }
             }
         }
-        //配列を返す手段募集
         
         //空いてる部分にランダムに数字を入れる
         //空いている場所の判定
         //空いている場所を確認してその上の部分から数字を取る
         //その後
+        
+        //デバッグの方法がわかるまで一時的な措置
+        //結局は不明
+        Random rnd = new Random();
+        int n = rnd.nextInt(MaxNumber+1);
+        PanelNumber[a][0] = n;
+        n = rnd.nextInt(MaxNumber+1);
+        PanelNumber[c][0] = n;
+        
+        return PanelNumber;
     }
     
+    
 }
-
-
-
-
-
-
